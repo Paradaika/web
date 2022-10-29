ef2762ef53995fe508c11ddec80c6bb9aba01c94
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-29 12:40:32 +0200

    TEST - should show Paradaika

--- src/service/ui/layout/Header/__test__/Header.test.js
+++ src/service/ui/layout/Header/__test__/Header.test.js
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { Header } from '../Header';
 
 describe('Header', () => {
   it('should render header', () => {
-    render(Header);
+    render(<Header />);
+  });
+
+  it('should show Paradaika', () => {
+    render(<Header />);
+
+    const title = screen.getByRole('heading');
+
+    expect(title.textContent).toBe('Paradaika');
   });
 });
