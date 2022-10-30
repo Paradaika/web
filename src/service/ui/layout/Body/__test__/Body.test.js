25835dbfe52cbf24f546ad280a8632f1fc2f2ac1
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-30 19:48:16 +0100

    TEST - should render body component

---
+++ src/service/ui/layout/Body/__test__/Body.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import { Body } from '../Body';
+
+describe('Body', () => {
+  it('should render body component', () => {
+    render(<Body />);
+  });
+});
