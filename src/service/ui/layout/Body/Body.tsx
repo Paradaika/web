fb1e88c069d0a5fc3ed2c3b00089764d1ce59615
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-31 22:20:00 +0100

    ADD - count down

--- src/service/ui/layout/Body/Body.tsx
+++ src/service/ui/layout/Body/Body.tsx
@@ -1,3 +1,21 @@
+import { useCallback, useEffect, useState } from 'react';
+
 export const Body = () => {
-  return <div></div>;
+  const [count, setCount] = useState<number>(25);
+
+  const countDown = () => {
+    setCount(prevNumber => {
+      return prevNumber - 1;
+    });
+  };
+
+  const countDownHandler = useCallback(() => {
+    if (count! > 0) countDown();
+  }, [count]);
+
+  useEffect(() => {
+    setTimeout(() => countDownHandler(), 1000);
+  }, [count, countDownHandler]);
+
+  return <div>{count}</div>;
 };
