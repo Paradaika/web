c0cd341230444db58cc55757090328efbd0fad93
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-01 19:57:45 +0100

    ADD - Default theme

---
+++ src/domain/styles/themes.ts
@@ -0,0 +1,10 @@
+const defaultTheme = {
+  background: '#2b272f',
+  main: '#ff9900',
+  secondary: '#63B2FF',
+  success: '#50D774',
+  error: '#FF262A',
+  grey: '#6A676C'
+};
+
+export const themes = { defaultTheme };
