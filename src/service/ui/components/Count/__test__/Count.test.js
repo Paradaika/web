9904e6635b85102375632f7a29efa428dd8af622
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-02 22:05:52 +0100

    TEST - should render Count component

---
+++ src/service/ui/components/Count/__test__/Count.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Count } from '../Count';
+
+describe('Count', () => {
+  it('should render Count component', () => {
+    render(<Count />);
+  });
+});
