087fc81f25f046c0c0e06cabaa3621edb3b83e11
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-03 23:24:56 +0100

    TEST - should be a play button

--- src/service/ui/components/Count/__test__/Count.test.js
+++ src/service/ui/components/Count/__test__/Count.test.js
@@ -1,8 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Count } from '../Count';
 
 describe('Count', () => {
   it('should render Count component', () => {
     render(<Count />);
+  });
+
+  it('should be a play button', () => {
+    render(<Count />);
+
+    const playButton = screen.getByRole('button', {
+      name: /play/i
+    });
+
+    expect(playButton).toBeInTheDocument();
   });
 });
