abd7dd7e69aad10c8e6c92bb0c56ed7037302911
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-07 23:29:29 +0100

    ADD - Time

---
+++ src/domain/Time/Time.ts
@@ -0,0 +1,23 @@
+export class Time {
+  private rawSeconds: number;
+  constructor(seconds: number) {
+    this.rawSeconds = seconds;
+  }
+
+  /**
+   * getMinutes
+   */
+  public getMinutes() {
+    const rawMinutes = this.rawSeconds / 60;
+    const formattedMinutes = rawMinutes.toString().split('.')[0];
+    return formattedMinutes;
+  }
+
+  /**
+   * getSeconds
+   */
+  public getSeconds() {
+    const rawSeconds = this.rawSeconds % 60;
+    return rawSeconds.toString();
+  }
+}
