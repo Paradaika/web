d70746e80b9c5339efe004ba9294f0734acb6a7a
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-07 23:34:30 +0100

    TEST - initial value 25 should render 25 min and 0 s

--- src/domain/Time/Time.test.js
+++ src/domain/Time/Time.test.js
@@ -1,10 +1,19 @@
 import { Time } from './Time';
 
+import { INITIAL_VALUE_25 } from 'domain/initialValues/initialValues';
+
 describe('Time', () => {
   it('should convert 61s in 1 min and 1s', () => {
     const time = new Time(61);
 
     expect(time.getMinutes()).toBe('1');
     expect(time.getSeconds()).toBe('1');
+  });
+
+  it('initial value 25 should render 25 min and 0 s', () => {
+    const time = new Time(INITIAL_VALUE_25 / 100);
+
+    expect(time.getMinutes()).toBe('25');
+    expect(time.getSeconds()).toBe('0');
   });
 });
