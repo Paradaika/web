29e9dca5387aee1b4003bf4e6014c4967950d8ba
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-10 20:16:49 +0100

    ADD - Styles Timer

---
+++ src/service/ui/components/Count/Timer/Timer.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledTimer = styled('div')`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const Styles = { StyledTimer };
--- src/service/ui/components/Count/Timer/Timer.tsx
+++ src/service/ui/components/Count/Timer/Timer.tsx
@@ -1,17 +1,19 @@
 import { Time } from 'domain/Time/Time';
 
+import { Styles } from './Timer.styles';
 interface Props {
   time: Time;
 }
 export const Timer = ({ time }: Props) => {
   return (
-    <div aria-label="count" className="count">
+    <Styles.StyledTimer aria-label="count">
       <p aria-label="minutes" className="minutes">
         {time.getMinutes()}
       </p>
+      <p>:</p>
       <p aria-label="seconds" className="seconds">
         {time.getSeconds()}
       </p>
-    </div>
+    </Styles.StyledTimer>
   );
 };
