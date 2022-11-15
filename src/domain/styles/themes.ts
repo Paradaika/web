ed97d3ff8af38343edbcde43d87fb0a7a4d7b59b
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-15 20:51:12 +0100

    ADD - ITheme interface

--- src/domain/styles/themes.ts
+++ src/domain/styles/themes.ts
@@ -1,10 +1,21 @@
-const defaultTheme = {
+export interface ITheme {
+  background: string;
+  main: string;
+  secondary: string;
+  success: string;
+  error: string;
+  grey: string;
+  fontFamily: string;
+}
+
+const defaultTheme: ITheme = {
   background: '#2b272f',
   main: '#ff9900',
   secondary: '#63B2FF',
   success: '#50D774',
   error: '#FF262A',
-  grey: '#6A676C'
+  grey: '#6A676C',
+  fontFamily: 'Roboto Mono, monospace'
 };
 
 export const themes = { defaultTheme };
