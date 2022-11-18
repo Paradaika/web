7998139003177cb3ea7ed50739fb04e436e803a3
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-18 19:34:50 +0100

    TEST - should have default color main (orangish)

--- src/service/ui/components/Button/StyledButton.test.js
+++ src/service/ui/components/Button/StyledButton.test.js
@@ -2,10 +2,19 @@ import { Styles } from './Button.styles';
 
 import { themes } from 'domain/styles/themes';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('Styled buttons', () => {
   it('should render styled button', () => {
     render(<Styles.StyledButton></Styles.StyledButton>);
+  });
+
+  it('should have default color main (orangish)', () => {
+    const BUTTON_TEXT = 'My Button';
+    render(<Styles.StyledButton theme={themes.defaultTheme}>{BUTTON_TEXT}</Styles.StyledButton>);
+
+    const button = screen.getByRole('button');
+
+    expect(button).toHaveStyle(`color: ${themes.defaultTheme.main}`);
   });
 });
