37a37636f2251aa7b7fcfb638dbcf2298c0bcc74
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-19 18:48:38 +0100

    ADD - Todo input functionality

--- src/service/ui/components/TodoListComponent/TodoInput/TodoInput.tsx
+++ src/service/ui/components/TodoListComponent/TodoInput/TodoInput.tsx
@@ -1,18 +1,31 @@
 import React, { useRef } from 'react';
+import { ITask } from '../TodoListComponent';
 import { TodoInputStyles } from './TodoInput.styles';
 
-export const TodoInput = () => {
+import { v4 as uuidGenerator } from 'uuid';
+
+interface Props {
+  onAdd: (task: ITask) => void;
+}
+
+export const TodoInput = ({ onAdd }: Props) => {
   const todoInputRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
   const todoFormHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('todoInputRef', todoInputRef.current?.value);
+
+    const task: ITask = {
+      id: uuidGenerator(),
+      isDone: false,
+      task: todoInputRef.current?.value || ''
+    };
+    onAdd(task);
     formRef.current?.reset();
   };
   return (
     <TodoInputStyles.StyledForm onSubmit={todoFormHandler} ref={formRef}>
-      <input name="todo" ref={todoInputRef} type="text" />
+      <input maxLength={30} name="todo" ref={todoInputRef} type="text" />
       <label htmlFor="todo">Insert tasks</label>
     </TodoInputStyles.StyledForm>
   );
--- src/service/ui/components/TodoListComponent/TodoListComponent.tsx
+++ src/service/ui/components/TodoListComponent/TodoListComponent.tsx
@@ -3,13 +3,27 @@ import { ThemeProvider } from 'styled-components';
 import TodoInput from 'service/ui/components/TodoListComponent/TodoInput';
 
 import { TodoListComponentStyles } from './TodoListComponent.styles';
+import { useState } from 'react';
+
+export interface ITask {
+  id: string;
+  task: string;
+  isDone: boolean;
+}
 
 export const TodoListComponent = () => {
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+  const appendTask = (task: ITask) => {
+    setTodoList(currentTaskList => [task, ...currentTaskList]);
+  };
   return (
     <ThemeProvider theme={themes.defaultTheme}>
       <TodoListComponentStyles.TodoListComponentContainer>
         <TodoListComponentStyles.TodoListHeader>Todo List</TodoListComponentStyles.TodoListHeader>
-        <TodoInput />
+        <TodoInput onAdd={appendTask} />
+        {todoList.map(task => {
+          return <p key={task.id}>{task.task}</p>;
+        })}
       </TodoListComponentStyles.TodoListComponentContainer>
     </ThemeProvider>
   );
