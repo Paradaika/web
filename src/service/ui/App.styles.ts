b3978bf2255bd264fb0d45151295ced727377d48
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-19 18:16:52 +0100

    ADD - default input styles

--- src/service/ui/App.styles.ts
+++ src/service/ui/App.styles.ts
@@ -39,7 +39,46 @@ h1 {
 h2{
   font-weight: 300;
 }
+
+form{
+  position: relative;
+}
+
+input {
+  height: 24px;
+  border-radius: 10px;
+  border: 3px solid rgba(255, 255, 255, 0.38);
+  background-color: rgba(255, 255, 255, 0.95);
+  background-color: transparent;
+  color: white;
+  position: absolute;
+}
+
+input:focus-visible {
+  outline-color: transparent;
+}
+form h3 {
+  margin-bottom: 3rem;
+}
+
+
+label {
+  position: relative;
+  bottom: -5px;
+  right: -10px;
+  transition: 0.3s;
+}
+
+input:hover + label,
+input:focus-visible + label,
+label:hover {
+  bottom: 30px;
+}
+
+
+
 `;
+
 const StyledApp = styled('div')`
   flex: 1;
   display: flex;
