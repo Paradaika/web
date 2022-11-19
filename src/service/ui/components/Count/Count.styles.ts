f156396fe549d0fbc71f2aa25931f05261b15b18
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-19 13:20:51 +0100

    REFACTOR - Counter styles

--- src/service/ui/components/Count/Count.styles.ts
+++ src/service/ui/components/Count/Count.styles.ts
@@ -1,14 +1,16 @@
+import { ITheme } from '@/domain/styles/themes';
 import styled from 'styled-components';
 
 interface Props {
   counterPercentage: string;
+  theme: ITheme;
 }
 
 const StyledCount = styled('div')<Props>`
   align-items: center;
   aspect-ratio: 1/1;
-  background: radial-gradient(closest-side, #2b272f 97%, transparent 0 99.9%, #32312b 0),
-    conic-gradient(#50d774 ${props => props.counterPercentage}, #2b272f 0);
+  background: radial-gradient(closest-side, ${prop => prop.theme.background} 97%, transparent 0 99.9%, ${prop => prop.theme.glassBorderColor} 0),
+    conic-gradient(${prop => prop.theme.success} ${props => props.counterPercentage}, ${prop => prop.theme.background} 0);
   border-radius: 50%;
   display: flex;
   display: grid;
