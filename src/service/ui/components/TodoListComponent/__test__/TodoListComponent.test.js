e541bba8914261b523ac22a400e8c7046d535a7b
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-21 21:00:24 +0100

    TEST - should add task in the component

--- src/service/ui/components/TodoListComponent/__test__/TodoListComponent.test.js
+++ src/service/ui/components/TodoListComponent/__test__/TodoListComponent.test.js
@@ -1,8 +1,30 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TodoListComponent } from '../TodoListComponent';
 
 describe('Todo List Component', () => {
   it('should render TodoListComponent', () => {
     render(<TodoListComponent />);
+  });
+  it('should be input component', () => {
+    render(<TodoListComponent />);
+
+    const inputComponent = screen.getByRole('textbox');
+
+    expect(inputComponent).toBeInTheDocument();
+  });
+
+  it('should add task in the component', async () => {
+    render(<TodoListComponent />);
+    const INPUT_TEXT = 'Task_1';
+    const inputComponent = screen.getByRole('textbox');
+
+    userEvent.type(inputComponent, INPUT_TEXT);
+    userEvent.type(inputComponent, '{enter}');
+
+    const task = await screen.findByText(INPUT_TEXT);
+
+    expect(task).toBeInTheDocument();
+    expect(task).toHaveTextContent(INPUT_TEXT);
   });
 });
