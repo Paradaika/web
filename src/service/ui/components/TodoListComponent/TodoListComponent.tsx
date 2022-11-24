6ab5674c2e9ce4e10397a458aa5cb9d93cb3769e
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-24 19:55:55 +0100

    ADD - Todo list sorting

--- src/service/ui/components/TodoListComponent/TodoListComponent.tsx
+++ src/service/ui/components/TodoListComponent/TodoListComponent.tsx
@@ -18,11 +18,17 @@ export const TodoListComponent = () => {
     setTodoList(currentTaskList => [task, ...currentTaskList]);
   };
 
+  const sortDoneLast = (a: ITask, b: ITask) => {
+    return a.isDone === b.isDone ? 0 : a.isDone ? 1 : -1;
+  };
+
   const changeTaskDoneStatus = (taskId: string) => {
     setTodoList(prevList => {
       const selectedTask = prevList.filter(task => task.id === taskId);
       const notSelectedTasks = prevList.filter(task => task.id !== taskId);
-      return [...notSelectedTasks, { ...selectedTask[0], isDone: !selectedTask[0].isDone }];
+      const allTasks = [...notSelectedTasks, { ...selectedTask[0], isDone: !selectedTask[0].isDone }];
+
+      return allTasks.sort(sortDoneLast);
     });
   };
   return (
