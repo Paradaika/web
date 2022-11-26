f400e19730debdc4e677cf117e09bee3a726bdca
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-26 13:22:43 +0100

    TEST - should render entered task text

--- src/service/ui/components/TodoListComponent/TaskItem/__test__/TaskItem.test.tsx
+++ src/service/ui/components/TodoListComponent/TaskItem/__test__/TaskItem.test.tsx
@@ -1,7 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { TaskItem } from '../TaskItem';
 
 describe('Task Item Component', () => {
   it('should render TaskItemComponent', () => {});
   render(<TaskItem id="1" isDone={false} onCheck={() => {}} task={'Dummy Task'} />);
+
+  it('should render entered task text', () => {
+    const TASK_TEST = 'TASK_1';
+
+    render(<TaskItem id="1" isDone={false} onCheck={() => {}} task={TASK_TEST} />);
+
+    const taskText = screen.getByText(TASK_TEST);
+
+    expect(taskText).toHaveTextContent(TASK_TEST);
+  });
 });
