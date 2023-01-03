1f9935baa6f4257b4968381c20e1e46871bffbd1
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-01-03 19:39:46 +0100

    ADD - Counter component to body

--- src/service/ui/layout/Body/Body.tsx
+++ src/service/ui/layout/Body/Body.tsx
@@ -1,12 +1,13 @@
 import Count from 'service/ui/components/Count';
 import TodoListComponent from 'service/ui/components/TodoListComponent';
+import Counters from '../../components/Counters';
 
 import { BodyStyles } from './Body.styles';
 
 export const Body = () => {
   return (
     <BodyStyles.StyledBody>
-      <div className="counter">Counter</div>
+      <Counters />
       <BodyStyles.StyledCountContainer>
         <Count />
       </BodyStyles.StyledCountContainer>
