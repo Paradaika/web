aae9054ed5c08ae50526be7d6ec525e716ab9bc4
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-01-20 09:07:07 +0100

    ADD - dark & sort

--- src/domain/styles/themes.ts
+++ src/domain/styles/themes.ts
@@ -1,27 +1,29 @@
 export interface ITheme {
   background: string;
-  main: string;
-  secondary: string;
-  success: string;
+  black: string;
   error: string;
-  grey: string;
   fontFamily: string;
   glassBackgroundColor: string;
   glassBackgroundColorDark: string;
   glassBorderColor: string;
+  grey: string;
+  main: string;
+  secondary: string;
+  success: string;
 }
 
 const defaultTheme: ITheme = {
-  background: 'rgb(28,28,28)',
-  main: '#ff9900',
-  secondary: '#63B2FF',
-  success: '#50D774',
+  background: '#e0e0e0',
+  black: '020202',
   error: '#FF262A',
-  grey: '#6A676C',
   fontFamily: 'Roboto Mono, monospace',
   glassBackgroundColor: 'rgba(255, 255, 255, 0.1)',
   glassBackgroundColorDark: 'rgba(0, 0, 0, 0.2)',
-  glassBorderColor: 'rgba(255, 255, 255, 0.15)'
+  glassBorderColor: 'rgba(255, 255, 255, 0.15)',
+  grey: '#6A676C',
+  main: '#ff9900',
+  secondary: '#63B2FF',
+  success: '#50D774'
 };
 
 export const themes = { defaultTheme };
