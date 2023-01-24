4ab3f960b43314a4fa6a1f91d540804c17866638
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-01-24 23:27:17 +0100

    UPDATE - Timer styles

--- src/service/ui/components/Count/Timer/Timer.styles.ts
+++ src/service/ui/components/Count/Timer/Timer.styles.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const StyledTimer = styled('div')`
   display: flex;
   gap: 1rem;
+  background: #e0e0e0;
+  box-shadow: inset 20px 20px 60px #bebebe, inset -20px -20px 60px #ffffff;
+  border-radius: 50px;
+  padding: 0rem 4rem;
 `;
 
 const StyledCountNumbers = styled('p')`
