dbd8c4ca778f705428223ba227860e9559d95979
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-01-30 21:40:42 +0100

    ADD - Portals Mock

--- src/service/ui/layout/Body/__test__/Body.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import { Body } from '../Body';
-
-describe('Body', () => {
-  it('should render body component', () => {
-    render(<Body />);
-  });
-});
--- src/service/ui/layout/Body/__test__/Body.test.tsx
+++ src/service/ui/layout/Body/__test__/Body.test.tsx
@@ -3,14 +3,14 @@ import { ReactNode, ReactPortal } from 'react';
 
 import { render } from '@testing-library/react';
 
-import { App } from './App';
+import { Body } from '../Body';
 
-describe('App component', () => {
-  it('should render App component', () => {
-    ReactDOM.createPortal = jest.fn((element: React.ReactNode, node: Element | DocumentFragment) => {
+describe('Body', () => {
+  it('should render body component', () => {
+    ReactDOM.createPortal = jest.fn((element: ReactNode, node: Element | DocumentFragment) => {
       return element;
     }) as (children: ReactNode, container: Element | DocumentFragment, key?: string | null | undefined) => ReactPortal;
 
-    render(<App />);
+    render(<Body />);
   });
 });
