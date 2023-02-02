bef10e979706cfadd58541a2925651058601432c
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-02 21:15:25 +0100

    UPDATE - Timer Settings labels and style

--- src/service/ui/components/TimerSettings/TimerSettings.styles.ts
+++ src/service/ui/components/TimerSettings/TimerSettings.styles.ts
@@ -9,13 +9,32 @@ const StyledTimerSettingsContainer = styled('form')`
   box-shadow: 22px 22px 44px #b1b1b1, -22px -22px 44px #ffffff;
   display: flex;
   flex-direction: column;
-  height: 100px;
   justify-content: center;
   padding: 2rem;
   width: 500px;
+  gap: 1.4rem;
   .input {
     position: relative;
   }
 `;
 
-export const TimerSettingsStyles = { StyledTimerSettingsContainer };
+const StyledInputContainer = styled('div')`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 3rem;
+  align-items: center;
+
+  .input {
+    text-align: end;
+    width: 50px;
+  }
+  .label {
+    flex: 0.3;
+    font-size: 1.2rem;
+    font-weight: 400;
+    margin: 0;
+  }
+`;
+
+export const TimerSettingsStyles = { StyledTimerSettingsContainer, StyledInputContainer };
--- src/service/ui/components/TimerSettings/TimerSettings.tsx
+++ src/service/ui/components/TimerSettings/TimerSettings.tsx
@@ -5,9 +5,18 @@ import { TimerSettingsStyles } from './TimerSettings.styles';
 export const TimerSettings = () => {
   return (
     <TimerSettingsStyles.StyledTimerSettingsContainer>
-      <input className="input" id="work-time" max={60} min={0} name="work-time" type="number" />
-      <input className="input" id="short-rest" max={15} min={5} name="short-rest" type="number" />
-      <input className="input" id="long-rest" max={60} min={5} name="long-rest" type="number" />
+      <TimerSettingsStyles.StyledInputContainer>
+        <input className="input" id="work-time" max={60} min={0} name="work-time" type="number" />
+        <p className="label">Work Time</p>
+      </TimerSettingsStyles.StyledInputContainer>
+      <TimerSettingsStyles.StyledInputContainer>
+        <input className="input" id="short-rest" max={15} min={5} name="short-rest" type="number" />
+        <p className="label">Short Rest</p>
+      </TimerSettingsStyles.StyledInputContainer>
+      <TimerSettingsStyles.StyledInputContainer>
+        <input className="input" id="long-rest" max={60} min={5} name="long-rest" type="number" />
+        <p className="label">Long Rest</p>
+      </TimerSettingsStyles.StyledInputContainer>
     </TimerSettingsStyles.StyledTimerSettingsContainer>
   );
 };
