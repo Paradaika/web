c0666da12928f062bdc8ae65419094893dc053f1
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-07 20:51:58 +0100

    UPDATE - Modal styles

--- src/service/ui/components/Modal/Modal.styles.ts
+++ src/service/ui/components/Modal/Modal.styles.ts
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 const StyledModal = styled('div')`
-  background: #e0e0e0;
-  background: #e0e0e0;
-  background: linear-gradient(145deg, #cacaca, #f0f0f0);
-  border-radius: 50px;
-  box-shadow: 22px 22px 44px #b1b1b1, -22px -22px 44px #ffffff;
-  padding: 2rem;
   position: relative;
   width: fit-content;
 `;
