a1f99725ab73ad4101d14ab2df3585a82a3465b0
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-12 14:19:59 +0100

    UPDATE - UseCount to use context

--- src/service/ui/components/Count/useCount.tsx
+++ src/service/ui/components/Count/useCount.tsx
@@ -2,11 +2,9 @@ import { useCallback, useEffect, useState } from 'react';
 
 import { subtract } from 'domain/operations/subtract';
 
-import { INITIAL_VALUE_25 } from 'domain/initialValues/initialValues';
-
-export const useCount = () => {
-  const [count, setCount] = useState(INITIAL_VALUE_25);
+export const useCount = (initialMilliseconds: number) => {
   const [isPaused, setIsPaused] = useState(true);
+  const [count, setCount] = useState(initialMilliseconds);
 
   const countdown = () => {
     setCount(subtract);
@@ -21,7 +19,7 @@ export const useCount = () => {
   };
 
   const reset = () => {
-    setCount(INITIAL_VALUE_25);
+    setCount(initialMilliseconds);
   };
 
   const resetHandler = () => {
@@ -29,15 +27,19 @@ export const useCount = () => {
   };
 
   const countDownHandler = useCallback(() => {
-    if (count! > 0 && !isPaused) countdown();
+    if (count === 0) {
+      setIsPaused(true);
+    }
+    if (count > 0 && !isPaused) countdown();
   }, [count, isPaused]);
-
-  const progressPercentage: number = (count / INITIAL_VALUE_25) * 100;
-
-  const fixedProgressPercentage = progressPercentage.toFixed();
 
   useEffect(() => {
     setTimeout(() => countDownHandler(), 10);
   }, [count, countDownHandler]);
-  return { pauseHandler, resetHandler, playHandler, count, isPaused, fixedProgressPercentage };
+
+  useEffect(() => {
+    setCount(initialMilliseconds);
+  }, [initialMilliseconds]);
+
+  return { pauseHandler, resetHandler, playHandler, count, isPaused };
 };
