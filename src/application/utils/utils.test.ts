11c2326229d2dd68052b2580e3d398cd4138c065
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-22 20:34:44 +0100

    TEST - should return work time on first round

---
+++ src/application/utils/utils.test.ts
@@ -0,0 +1,17 @@
+import { ISettings } from '@/domain/interfaces/ISettings';
+import { getInitialTime } from './getInitialTime';
+
+describe('Get initial time', () => {
+  it('should return work time on first round', () => {
+    const fakeSettings: ISettings = {
+      longRest: 1,
+      shortRest: 2,
+      workTime: 3
+    };
+    const fakeCount = 0;
+
+    const initialTime = getInitialTime({ settings: fakeSettings, timerCount: fakeCount });
+
+    expect(initialTime).toBe(fakeSettings.workTime);
+  });
+});
