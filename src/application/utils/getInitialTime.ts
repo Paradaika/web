df29e9479c3dae773378bf325a4363e0cd83b392
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-22 20:34:06 +0100

    FIX - getInitialTime

--- src/application/utils/getInitialTime.ts
+++ src/application/utils/getInitialTime.ts
@@ -5,6 +5,7 @@ interface IProps {
   settings: ISettings;
 }
 export const getInitialTime = ({ timerCount, settings }: IProps) => {
+  if (timerCount === 0) return settings.workTime;
   if (timerCount % 4 === 0) return settings.longRest;
   if (timerCount % 2 === 0) return settings.workTime;
   return settings.shortRest;
