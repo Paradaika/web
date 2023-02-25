3e7af6a3f12b0b1866c2136f65c8365e5fbd5bf5
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-02-25 15:23:00 +0100

    TEST - should move completed task to the last position

--- cypress/e2e/task-list.cy.ts
+++ cypress/e2e/task-list.cy.ts
@@ -38,4 +38,21 @@ describe('Todo List', () => {
     //cy.get('.sc-ksBlkl').should('have.color', 'disabled')
     cy.get('.done');
   });
+
+  it('should move completed task to the last position ', () => {
+    cy.visit('http://localhost:3000');
+    const todoListHeader = cy.contains(/Todo list/i);
+    const todoText = 'TEST 1';
+    const todoText2 = 'TEST 2';
+
+    todoListHeader.click();
+    cy.get('input[name="todo"]')
+      .type(todoText + '{enter}', { force: true })
+      .type(todoText2 + '{enter}', { force: true });
+    cy.get('.sc-idXgbr > :nth-child(2)').find('button').click();
+
+    cy.get('.sc-idXgbr').last().find('.sc-hBxehG').should('contain', todoText2);
+
+    //cy.get('.sc-ksBlkl').should('have.color', 'disabled')
+  });
 });
