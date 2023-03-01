184a280957ad450f376849dd4b06ad452897959d
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-01 20:28:32 +0100

    TEST - should find play button

--- cypress/e2e/Counter.cy.ts
+++ cypress/e2e/Counter.cy.ts
@@ -4,4 +4,7 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000');
   });
   it('should visit page', () => {});
+  it('should find play button ', () => {
+    cy.get('[data-testid="play-button"]');
+  });
 });
