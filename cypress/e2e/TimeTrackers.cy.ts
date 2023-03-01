9eda940454adea776295b414bb794c78ad4dcdb4
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-01 20:24:35 +0100

    UPDATE - template spec test

--- cypress/e2e/TimeTrackers.cy.ts
+++ cypress/e2e/TimeTrackers.cy.ts
@@ -1,4 +1,5 @@
 describe('template spec', () => {
+  beforeEach(() => {});
   it('should be a Counters section', () => {
     cy.visit('http://localhost:3000');
     cy.contains(/Counters/i);
@@ -22,5 +23,19 @@ describe('template spec', () => {
     cy.get('.timeTrackerInput').type(counter1 + '{enter}', { force: true });
 
     cy.contains(counter1);
+  });
+
+  it('should start a timer ', () => {
+    cy.visit('http://localhost:3000');
+    const counters = cy.contains(/Counters/i);
+    const counter1 = 'COUNTER 1';
+
+    counters.click();
+    cy.get('.timeTrackerInput').type(counter1 + '{enter}', { force: true });
+    cy.contains(counter1);
+    cy.contains(/play/i).click();
+    setTimeout(() => {}, 3000);
+
+    cy.get('.sc-fLcnxK > :nth-child(1)').should('contain', '3');
   });
 });
